b8b910b851cd35129ed01137be385f7c70fbde86
Mohit Nikumbh <nikumbhmohit@gmail.com>
2022-06-23 15:25:57 +0530

    first heroku deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return 'starting machine learning project'
+    return 'we succesfully create ci cd pipeline.'
 
 if __name__=="__main__":
     app.run(debug=True)
